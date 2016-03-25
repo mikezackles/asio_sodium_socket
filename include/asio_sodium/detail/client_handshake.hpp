741d7c69531055234bd3fd8c6f6b265d1d214f66
Zachary Michaels <mikezackles@gmail.com>
2016-03-25 09:29:58 -0400

    Move helper methods out of session_data
    This helps split things up for testing and makes things a bit more
    modular in general.

--- include/asio_sodium/detail/client_handshake.hpp
+++ include/asio_sodium/detail/client_handshake.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "asio_types.hpp"
+#include "handshake_hello.hpp"
+#include "handshake_response.hpp"
 #include "session_data.hpp"
 
 #include <asio/coroutine.hpp>
+#include <asio/read.hpp>
+#include <asio/write.hpp>
 #include <asio/yield.hpp>
 
 namespace asio_sodium {
@@ -41,10 +45,10 @@ namespace detail {
 
       reenter (this) {
         yield socket_.async_connect(endpoint_, std::move(*this));
-        session_.make_hello();
-        yield session_.send_hello(socket_, std::move(*this));
-        yield session_.await_hello_response(socket_, std::move(*this));
-        if (session_.process_hello_response()) {
+        make_hello();
+        yield send_hello(socket_, std::move(*this));
+        yield await_hello_response(socket_, std::move(*this));
+        if (process_hello_response()) {
           on_success_();
         } else {
           // TODO custom error_code (this one is defaulted to indicate no error)
@@ -52,7 +56,61 @@ namespace detail {
         }
       }
     }
+
   private:
+    void
+    make_hello()
+    noexcept {
+      handshake_hello hello(session_.hello_buffer);
+      hello.set_public_key(session_.local_public_key);
+      hello.generate_reply_nonce();
+      hello.copy_reply_nonce(session_.decrypt_nonce);
+      hello.encrypt_to(session_.remote_public_key);
+    }
+
+    template <typename Callback>
+    void
+    send_hello(socket_type& socket, Callback&& callback)
+    noexcept {
+      asio::async_write(
+        socket
+      , asio::buffer(session_.hello_buffer)
+      , std::forward<Callback>(callback)
+      );
+    }
+
+    template <typename Callback> void
+    await_hello_response(
+      socket_type& socket, Callback&& callback
+    )
+    noexcept {
+      asio::async_read(
+        socket
+      , asio::buffer(session_.hello_response_buffer)
+      , std::forward<Callback>(callback)
+      );
+    }
+
+    bool
+    process_hello_response()
+    noexcept {
+      auto response = handshake_response::decrypt(
+        session_.hello_response_buffer
+      , session_.decrypt_nonce
+      , session_.remote_public_key
+      , session_.local_private_key
+      );
+
+      if (!response) {
+        return false;
+      }
+
+      response->copy_reply_nonce(session_.encrypt_nonce);
+      response->copy_followup_nonce(session_.decrypt_nonce);
+
+      return true;
+    }
+
     endpoint_type endpoint_;
     session_data& session_;
     socket_type& socket_;
--- include/asio_sodium/detail/server_handshake.hpp
+++ include/asio_sodium/detail/server_handshake.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "asio_types.hpp"
+#include "handshake_hello.hpp"
+#include "handshake_response.hpp"
+#include "session_data.hpp"
 
 #include <asio/coroutine.hpp>
+#include <asio/read.hpp>
+#include <asio/write.hpp>
 #include <asio/yield.hpp>
 
 namespace asio_sodium {
@@ -40,17 +45,17 @@ namespace detail {
       }
 
       reenter (this) {
-        yield session_.await_hello(socket_, std::move(*this));
+        yield await_hello(socket_, std::move(*this));
         // TODO custom error_codes
-        if (!session_.process_hello(authenticator_)) {
+        if (!process_hello(authenticator_)) {
           on_error_(ec, bytes);
           yield break;
         }
-        if (!session_.make_hello_response()) {
+        if (!make_hello_response()) {
           on_error_(ec, bytes);
           yield break;
         }
-        yield session_.send_hello_response(
+        yield send_hello_response(
           socket_
         , std::move(*this)
         );
@@ -59,6 +64,89 @@ namespace detail {
     }
 
   private:
+    template <typename Callback>
+    void
+    await_hello(socket_type& socket, Callback&& callback)
+    noexcept {
+      asio::async_read(
+        socket
+      , asio::buffer(session_.hello_buffer)
+      , std::forward<Callback>(callback)
+      );
+    }
+
+    template <typename Auth>
+    bool
+    process_hello(Auth&& auth)
+    noexcept {
+      auto hello =
+        handshake_hello::decrypt(
+          session_.hello_buffer
+        , session_.local_public_key
+        , session_.local_private_key
+        )
+      ;
+      if (!hello) {
+        return false;
+      }
+      auto public_key = hello->public_key_span();
+      // Look up the public key and make sure it's authorized
+      if (!auth(public_key)) {
+        return false;
+      }
+      std::copy(
+        public_key.begin()
+      , public_key.end()
+      , session_.remote_public_key.begin()
+      );
+      hello->copy_reply_nonce(session_.encrypt_nonce);
+      return true;
+    }
+
+    bool
+    make_hello_response()
+    noexcept {
+      handshake_response response{session_.hello_response_buffer};
+
+      response.generate_reply_nonce();
+      response.copy_reply_nonce(session_.decrypt_nonce);
+
+      nonce temp_followup_nonce;
+
+      response.generate_followup_nonce();
+      response.copy_followup_nonce(temp_followup_nonce);
+
+      if (
+        !response.encrypt_to(
+          session_.encrypt_nonce
+        , session_.remote_public_key
+        , session_.local_private_key
+        )
+      ) {
+        return false;
+      }
+
+      std::copy(
+        temp_followup_nonce.begin()
+      , temp_followup_nonce.end()
+      , session_.encrypt_nonce.begin()
+      );
+
+      return true;
+    }
+
+    template <typename Callback> void
+    send_hello_response(
+      socket_type& socket, Callback&& callback
+    )
+    noexcept {
+      asio::async_write(
+        socket
+      , asio::buffer(session_.hello_response_buffer)
+      , std::forward<Callback>(callback)
+      );
+    }
+
     session_data& session_;
     socket_type& socket_;
     Authenticator authenticator_;
