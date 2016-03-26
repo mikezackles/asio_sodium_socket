eccfcd910171259115b4c32308ea47f697f8fdac
Zachary Michaels <mikezackles@gmail.com>
2016-03-26 14:25:13 -0400

    Go ahead and use Apache 2.0 for the whole project

--- include/asio_sodium/detail/handshake_response.hpp
+++ include/asio_sodium/detail/handshake_response.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Zachary Michaels
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef ASIO_SODIUM_8a4c094b_6c1f_40d5_acb8_7b1652a8fde6
 #define ASIO_SODIUM_8a4c094b_6c1f_40d5_acb8_7b1652a8fde6
 
